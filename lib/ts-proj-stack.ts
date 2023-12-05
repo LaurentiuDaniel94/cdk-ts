2aae0621ffb83d71ce37cbd4e87140e9d3c8b3f9
Laurentiu Daniel Ginghina <laurentiuginghina@outlook.com>
2023-12-05 21:05:24 +0200

    wip

--- lib/ts-proj-stack.ts
+++ lib/ts-proj-stack.ts
@@ -1,16 +1,45 @@
 import * as cdk from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+
+export class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+
+    new Bucket(this, 'myL3Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(expiration)
+  }
+      ]
+    })
+  }
+}
 
 export class TsProjStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create an S3 bucket with a lifecycle rule to delete objects after 1 day with L1 construct
+    new CfnBucket(this, 'myL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: 1,
+          status: 'Enabled'
+        }]
+      }
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsProjQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // create an S3 bucket with a lifecycle rule to delete objects after 2 days with L2 construct
+    new Bucket(this, 'myL2Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(2)
+      }]
+    })
+
+    new L3Bucket(this, 'myL3Bucket', 3);
   }
 }
